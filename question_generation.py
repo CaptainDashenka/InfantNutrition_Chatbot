4fdd493ac1cd2227d0e1ccb01adc96e1d0dfcce0
CaptainDashenka <david@metatron.com.hk>
2024-08-05 13:46:56 +0800

    limited the number of questions generated.

--- question_generation.py
+++ question_generation.py
@@ -6,7 +6,7 @@ reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
 docs = reader.load_data()
 
 data_generator = DatasetGenerator.from_documents(docs)
-
-eval_questions = data_generator.generate_questions_from_nodes()
+# the numer of questions generated had to be limited to avoid API call token limits by Open AI
+eval_questions = data_generator.generate_questions_from_nodes(num=200)
 
 print(eval_questions)
